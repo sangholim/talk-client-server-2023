fa331c4a66b319139bc8dca0781cd07ae2b3bf62
sangholim <55565835+sangholim@users.noreply.github.com>
2023-03-14 15:19:30 +0900

    feat: 프로젝트 추가 (#2)
    * feat: 프로젝트 생성
    
    * feat: docker-compose.yaml 추가

---
+++ src/test/kotlin/com/talk/service/ServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.talk.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
