1c1c73a04608dd9c300f38ef2e3b0d11a189f32e
sangholim <55565835+sangholim@users.noreply.github.com>
2023-03-15 17:17:40 +0900

    refactor: oauth2-client 승인 설정 수정 (#12)
    * refactor: application-local.yml 수정
    - redirect_uri 수정
    
    * refactor: SecurityConfig.kt 수정
    - session stateless 로 변경
    - requestCache cookie 방식으로 변경
    
    * refactor: HttpCookieOauth2AuthorizationRequestRepository 수정
    - 승인 요청이 일어나는 경우, redirect_uri 쿠키로 저장

--- src/main/kotlin/com/talk/client/service/config/SecurityConfig.kt
+++ src/main/kotlin/com/talk/client/service/config/SecurityConfig.kt
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.savedrequest.CookieRequestCache
 
 
 @Configuration
@@ -16,9 +18,11 @@ class SecurityConfig(
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http.requestCache().requestCache(CookieRequestCache())
         http.oauth2Client { oauth2Client ->
             oauth2Client.authorizationCodeGrant().authorizationRequestRepository(httpCookieOauth2AuthorizationRequestRepository)
+
         }
 
         return http.build()
