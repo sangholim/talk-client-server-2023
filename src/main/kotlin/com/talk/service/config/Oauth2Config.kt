cc4fd6d9ade614bd9ca3fc309955f54613efb057
hoya <hybriddevslim@gmail.com>
2023-03-15 12:28:46 +0900

    refactor: Oauth2Config 수정 - authorizedClientManager 객체 생성 로직 추가 - authorizedClient 객체 생성 로직 추가

--- src/main/kotlin/com/talk/service/config/Oauth2Config.kt
+++ src/main/kotlin/com/talk/service/config/Oauth2Config.kt
@@ -1,25 +1,39 @@
 package com.talk.service.config
 
-import com.talk.service.oauth2.HttpCookieOAuth2AuthorizationRequestRepository
-import com.talk.service.oauth2.OAuth2AuthenticationFailureHandler
-import com.talk.service.oauth2.OAuth2AuthenticationSuccessHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction
+import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
 class Oauth2Config {
 
     @Bean
-    fun httpCookieOAuth2AuthorizationRequestRepository(): HttpCookieOAuth2AuthorizationRequestRepository =
-            HttpCookieOAuth2AuthorizationRequestRepository()
-
-
+    fun authorizedClientManager(
+            clientRegistrationRepository: ClientRegistrationRepository,
+            authorizedClientRepository: OAuth2AuthorizedClientRepository): OAuth2AuthorizedClientManager {
+        val authorizedClientProvider: OAuth2AuthorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+                .authorizationCode()
+                .refreshToken()
+                .clientCredentials()
+                .build()
+        val authorizedClientManager = DefaultOAuth2AuthorizedClientManager(
+                clientRegistrationRepository, authorizedClientRepository)
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
+        return authorizedClientManager
+    }
     @Bean
-    fun oAuth2AuthenticationFailureHandler(httpCookieOAuth2AuthorizationRequestRepository: HttpCookieOAuth2AuthorizationRequestRepository): OAuth2AuthenticationFailureHandler =
-            OAuth2AuthenticationFailureHandler(httpCookieOAuth2AuthorizationRequestRepository)
-
-    @Bean
-    fun oAuth2AuthenticationSuccessHandler(httpCookieOAuth2AuthorizationRequestRepository: HttpCookieOAuth2AuthorizationRequestRepository): OAuth2AuthenticationSuccessHandler =
-            OAuth2AuthenticationSuccessHandler(httpCookieOAuth2AuthorizationRequestRepository)
+    fun webClient(authorizedClientManager: OAuth2AuthorizedClientManager): WebClient {
+        val oauth2Client = ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
+        return WebClient.builder()
+                .apply(oauth2Client.oauth2Configuration())
+                .build()
+    }
 
 }
