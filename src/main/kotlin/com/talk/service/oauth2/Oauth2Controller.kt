d53f212785c17df05dbc6726115164828504969d
hoya <hybriddevslim@gmail.com>
2023-03-15 12:27:53 +0900

    feat: Oauth2Controller 추가 - oauth2/login 로직 추가

---
+++ src/main/kotlin/com/talk/service/oauth2/Oauth2Controller.kt
@@ -0,0 +1,26 @@
+package com.talk.service.oauth2
+
+import com.talk.service.util.CookieUtils
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class Oauth2Controller {
+
+    @GetMapping("/oauth2/login")
+    fun oauth2Login(@RegisteredOAuth2AuthorizedClient("sso") authorizedClient: OAuth2AuthorizedClient, response: HttpServletResponse): String {
+        val accessToken = authorizedClient.accessToken
+        val refreshToken = authorizedClient.refreshToken
+        accessToken?.run {
+            CookieUtils.addCookie(response, "accessToken", this.tokenValue, this.maxAge())
+        }
+        refreshToken?.run {
+            CookieUtils.addCookie(response, "refreshToken", this.tokenValue, this.maxAge())
+        }
+
+        return "index"
+    }
+}
