6257c11697596062ff76690900da1185461b5347
hoya <hybriddevslim@gmail.com>
2023-03-15 09:48:13 +0900

    refactor: SecurityConfig 수정 - session 관리 정책: stateless - 승인 로직 수정 - 인증 성공/실패 핸들러 수정

--- src/main/kotlin/com/talk/service/config/SecurityConfig.kt
+++ src/main/kotlin/com/talk/service/config/SecurityConfig.kt
@@ -1,15 +1,23 @@
 package com.talk.service.config
 
+import com.talk.service.oauth2.HttpCookieOAuth2AuthorizationRequestRepository
+import com.talk.service.oauth2.OAuth2AuthenticationFailureHandler
+import com.talk.service.oauth2.OAuth2AuthenticationSuccessHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+        private val httpCookieOAuth2AuthorizationRequestRepository: HttpCookieOAuth2AuthorizationRequestRepository,
+        private val oAuth2AuthenticationSuccessHandler: OAuth2AuthenticationSuccessHandler,
+        private val oAuth2AuthenticationFailureHandler: OAuth2AuthenticationFailureHandler
+) {
 
     @Bean
     @Throws(Exception::class)
@@ -17,7 +25,13 @@ class SecurityConfig {
         http.authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .oauth2Login()
+                .authorizationEndpoint { it.authorizationRequestRepository(httpCookieOAuth2AuthorizationRequestRepository) }
+                .successHandler(oAuth2AuthenticationSuccessHandler)
+                .failureHandler(oAuth2AuthenticationFailureHandler)
         return http.build()
     }
 }
