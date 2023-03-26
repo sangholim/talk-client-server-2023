2913d594af6a74d7fe675512893b9d6d47d1d342
hoya <hybriddevslim@gmail.com>
2023-03-27 02:58:53 +0900

    refactor: Oauth2Controller 수정 - /oauth2/logout 추가

--- src/main/kotlin/com/talk/client/service/oauth2/Oauth2Controller.kt
+++ src/main/kotlin/com/talk/client/service/oauth2/Oauth2Controller.kt
@@ -1,14 +1,21 @@
 package com.talk.client.service.oauth2
 
 import com.talk.client.service.util.CookieUtils
+import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
+import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
-class Oauth2Controller {
+class Oauth2Controller(
+        private val oauth2Service: Oauth2Service
+) {
 
     /**
      * oauth2 인증을 성공한 경우, 인증이 필요한 기본 화면으로 진입
@@ -24,5 +31,12 @@ class Oauth2Controller {
             CookieUtils.addCookie(response, Oauth2Constant.REFRESH_TOKEN, this.tokenValue, this.maxAge())
         }
         return "redirect:${Oauth2Constant.SUCCESS_URL}"
+    }
+
+    @PostMapping("/oauth2/logout", consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
+    fun oauth2Logout(@RegisteredOAuth2AuthorizedClient("sso") authorizedClient: OAuth2AuthorizedClient, request: HttpServletRequest, response: HttpServletResponse, authentication: Authentication): String {
+        oauth2Service.logout(authorizedClient)
+        SecurityContextLogoutHandler().logout(request, response, authentication)
+        return "redirect:/"
     }
 }
