1b42762c61086dfd7e9ead7b7f21031f2b178b20
hoya <hybriddevslim@gmail.com>
2023-03-26 13:54:32 +0900

    refactor: Oauth2Constant 수정 - ACCESS_TOKEN, REFRESH_TOKEN name 추가

--- src/main/kotlin/com/talk/client/service/oauth2/Oauth2Constant.kt
+++ src/main/kotlin/com/talk/client/service/oauth2/Oauth2Constant.kt
@@ -2,4 +2,6 @@ package com.talk.client.service.oauth2
 
 object Oauth2Constant {
     val LOGIN_URL = "/oauth2/login"
+    val ACCESS_TOKEN = "accessToken"
+    val REFRESH_TOKEN = "refreshToken"
 }
