293662c8140d240bf9ebd0bb32a396bc8dd006be
sangholim <55565835+sangholim@users.noreply.github.com>
2023-03-26 14:50:50 +0900

    feat: 접근 페이지 & 로그인후 페이지 추가 (#24)
    * chore: build.gradle.kts 수정
    - thymeleaf-dialect 의존성 라이브러리 추가
    
    * refactor: application-local.yml 수정
    - thymeleaf 설정 추가
    
    * feat: main.html 추가
    - 서비스 로그인 페이지 추가
    
    * feat: friends/main.html 추가
    - 서비스 로그인후 기본 페이지 추가
    
    * feat: EntryController 추가
    - 기본 로그인 페이지 dispatcher 추가
    
    * refactor: FriendController 수정
    - dispatcher 경로 추가
    
    * refactor: SecurityConfig 수정
    - root path 경로 권한 없어도 접근하도록 수정

--- src/main/kotlin/com/talk/client/service/friend/FriendController.kt
+++ src/main/kotlin/com/talk/client/service/friend/FriendController.kt
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping
 @Controller
 class FriendController {
 
+    /**
+     * 친구 리스트 화면 렌더링
+     * 기본 로그인후 이동하는 페이지
+     */
     @GetMapping("/friends")
-    fun getAll(authentication: Authentication) {
-        println("test: $authentication")
+    fun getAll(authentication: Authentication): String {
+        return "friends/main"
     }
 }
