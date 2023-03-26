951b989306177e6effa751f6b1fa2c1c89e1d24e
hoya <hybriddevslim@gmail.com>
2023-03-27 00:07:27 +0900

    refactor: SecurityConfig 수정 - requestCache -> 쿠키 설정 제거 - logout 처리되는 경우 설정 추가

--- src/main/kotlin/com/talk/client/service/config/SecurityConfig.kt
+++ src/main/kotlin/com/talk/client/service/config/SecurityConfig.kt
@@ -2,14 +2,13 @@ package com.talk.client.service.config
 
 import com.talk.client.service.oauth2.HttpCookieOauth2AuthorizationRequestRepository
 import com.talk.client.service.oauth2.Oauth2AuthenticationEntryPoint
+import com.talk.client.service.oauth2.Oauth2Constant
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.savedrequest.CookieRequestCache
-
 
 @Configuration
 @EnableWebSecurity
@@ -17,10 +16,9 @@ class SecurityConfig(
         private val httpCookieOauth2AuthorizationRequestRepository: HttpCookieOauth2AuthorizationRequestRepository,
         private val authenticationEntryPoint: Oauth2AuthenticationEntryPoint
 ) {
-     @Bean
+    @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        http.requestCache().requestCache(CookieRequestCache())
         http.oauth2Client { oauth2Client ->
             oauth2Client.authorizationCodeGrant().authorizationRequestRepository(httpCookieOauth2AuthorizationRequestRepository)
         }
@@ -29,8 +27,15 @@ class SecurityConfig(
             it.requestMatchers("/", "/oauth2/login").permitAll()
             it.anyRequest().authenticated()
         }
-         http.oauth2ResourceServer().jwt()
-         http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
+        http.oauth2ResourceServer().jwt()
+        http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
+
+        http.logout()
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies(Oauth2Constant.ACCESS_TOKEN, Oauth2Constant.REFRESH_TOKEN, "JSESSIONID")
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
         return http.build()
     }
 }
