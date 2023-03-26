0c42fdeebd59c7355b87b610e8306dd2b98a7501
hoya <hybriddevslim@gmail.com>
2023-03-26 12:44:32 +0900

    refactor: SecurityConfig 수정 - jwt 인증 로직 추가 - 인증 서버로부터 jwt key 정보를 이용하여 인증 수행 - 실패시 /oauth2/login 페이지로 이동

--- src/main/kotlin/com/talk/client/service/config/SecurityConfig.kt
+++ src/main/kotlin/com/talk/client/service/config/SecurityConfig.kt
@@ -1,6 +1,7 @@
 package com.talk.client.service.config
 
 import com.talk.client.service.oauth2.HttpCookieOauth2AuthorizationRequestRepository
+import com.talk.client.service.oauth2.Oauth2AuthenticationEntryPoint
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -13,18 +14,23 @@ import org.springframework.security.web.savedrequest.CookieRequestCache
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
-        private val httpCookieOauth2AuthorizationRequestRepository: HttpCookieOauth2AuthorizationRequestRepository
+        private val httpCookieOauth2AuthorizationRequestRepository: HttpCookieOauth2AuthorizationRequestRepository,
+        private val authenticationEntryPoint: Oauth2AuthenticationEntryPoint
 ) {
-
-    @Bean
+     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         http.requestCache().requestCache(CookieRequestCache())
         http.oauth2Client { oauth2Client ->
             oauth2Client.authorizationCodeGrant().authorizationRequestRepository(httpCookieOauth2AuthorizationRequestRepository)
-
         }
-
+        http.formLogin().disable()
+        http.authorizeHttpRequests {
+            it.requestMatchers("/oauth2/login").permitAll()
+            it.anyRequest().authenticated()
+        }
+         http.oauth2ResourceServer().jwt()
+         http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
         return http.build()
     }
 }
