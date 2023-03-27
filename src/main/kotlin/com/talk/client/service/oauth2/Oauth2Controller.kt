1ab43d513146c49965e793d96ac0f55ea9006d99
hoya <hybriddevslim@gmail.com>
2023-03-27 17:57:23 +0900

    refactor: Oauth2Controller 수정 - logout url 제거

--- src/main/kotlin/com/talk/client/service/oauth2/Oauth2Controller.kt
+++ src/main/kotlin/com/talk/client/service/oauth2/Oauth2Controller.kt
@@ -3,25 +3,19 @@ package com.talk.client.service.oauth2
 import com.talk.client.service.util.CookieUtils
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.springframework.http.MediaType
-import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
-class Oauth2Controller(
-        private val oauth2Service: Oauth2Service
-) {
+class Oauth2Controller {
 
     /**
      * oauth2 인증을 성공한 경우, 인증이 필요한 기본 화면으로 진입
      */
     @GetMapping("/oauth2/login")
-    fun oauth2Login(@RegisteredOAuth2AuthorizedClient("sso") authorizedClient: OAuth2AuthorizedClient, response: HttpServletResponse): String {
+    fun oauth2Login(@RegisteredOAuth2AuthorizedClient("sso") authorizedClient: OAuth2AuthorizedClient, request: HttpServletRequest, response: HttpServletResponse) {
         val accessToken = authorizedClient.accessToken
         val refreshToken = authorizedClient.refreshToken
         accessToken?.run {
@@ -30,13 +24,6 @@ class Oauth2Controller(
         refreshToken?.run {
             CookieUtils.addCookie(response, Oauth2Constant.REFRESH_TOKEN, this.tokenValue, this.maxAge())
         }
-        return "redirect:${Oauth2Constant.SUCCESS_URL}"
-    }
-
-    @PostMapping("/oauth2/logout", consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
-    fun oauth2Logout(@RegisteredOAuth2AuthorizedClient("sso") authorizedClient: OAuth2AuthorizedClient, request: HttpServletRequest, response: HttpServletResponse, authentication: Authentication): String {
-        oauth2Service.logout(authorizedClient)
-        SecurityContextLogoutHandler().logout(request, response, authentication)
-        return "redirect:/"
+        response.sendRedirect(Oauth2Constant.SUCCESS_URL)
     }
 }
