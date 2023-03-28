85c69d4608278272ff23fec673f3b0ca8541addd
hoya <hybriddevslim@gmail.com>
2023-03-28 10:29:05 +0900

    refactor: Oauth2Service 수정 - 토큰 파기 요청 로직 수정

--- src/main/kotlin/com/talk/client/service/oauth2/Oauth2Service.kt
+++ src/main/kotlin/com/talk/client/service/oauth2/Oauth2Service.kt
@@ -3,7 +3,7 @@ package com.talk.client.service.oauth2
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.convertValue
 import org.springframework.http.MediaType
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
+import org.springframework.security.oauth2.client.registration.ClientRegistration.ProviderDetails
 import org.springframework.stereotype.Service
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.web.reactive.function.client.WebClient
@@ -14,16 +14,9 @@ class Oauth2Service(
         private val objectMapper: ObjectMapper
 ) {
 
-    fun logout(authorizedClient: OAuth2AuthorizedClient) {
-        val uri = authorizedClient.clientRegistration.providerDetails.configurationMetadata["revocation_endpoint"] ?: return
-        val accessToken = authorizedClient.accessToken.tokenValue
-        val refreshToken = authorizedClient.refreshToken?.tokenValue
-        listOfNotNull(
-                Oauth2TokenRevokePayload.accessTokenOf(accessToken, clientRegistration = authorizedClient.clientRegistration),
-                Oauth2TokenRevokePayload.refreshTokenOf(refreshToken, clientRegistration = authorizedClient.clientRegistration)
-        ).forEach {
-            oauth2Client.revokeToken(uri.toString(), it)
-        }
+    fun revokeToken(providerDetails: ProviderDetails, payload: Oauth2TokenRevokePayload) {
+        val uri = providerDetails.configurationMetadata["revocation_endpoint"] ?: return
+        oauth2Client.revokeToken(uri.toString(), payload)
     }
 
     private fun WebClient.revokeToken(uri: String, payload: Oauth2TokenRevokePayload) {
