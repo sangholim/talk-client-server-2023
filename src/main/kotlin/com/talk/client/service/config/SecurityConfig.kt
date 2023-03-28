cdb730cd69d265423c70aa190e7dbe0da8fa6e72
hoya <hybriddevslim@gmail.com>
2023-03-28 10:31:12 +0900

    refactor: SecurityConfig 수정 - session stateless 처리 - 요청 캐쉬 쿠키 방식으로 변경

--- src/main/kotlin/com/talk/client/service/config/SecurityConfig.kt
+++ src/main/kotlin/com/talk/client/service/config/SecurityConfig.kt
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.savedrequest.CookieRequestCache
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +23,8 @@ class SecurityConfig(
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http.requestCache().requestCache(CookieRequestCache())
         http.oauth2Client { oauth2Client ->
             oauth2Client.authorizationCodeGrant().authorizationRequestRepository(httpCookieOauth2AuthorizationRequestRepository)
         }
