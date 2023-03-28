85eee403b8f0c9353dd2bade330b421a11ae9fd7
hoya <hybriddevslim@gmail.com>
2023-03-28 10:29:29 +0900

    refactor: Oauth2SsoLogoutHandler 수정 - 토큰 파기 요청 로직, 쿠키 제거 로직 수정

--- src/main/kotlin/com/talk/client/service/oauth2/Oauth2SsoLogoutHandler.kt
+++ src/main/kotlin/com/talk/client/service/oauth2/Oauth2SsoLogoutHandler.kt
@@ -4,22 +4,24 @@ import com.talk.client.service.util.CookieUtils
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.Authentication
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.web.authentication.logout.LogoutHandler
 
 class Oauth2SsoLogoutHandler(
         private val oauth2Service: Oauth2Service,
-        private val oAuth2AuthorizedClientRepository: OAuth2AuthorizedClientRepository
+        private val clientRegistrationRepository: ClientRegistrationRepository
 ) : LogoutHandler {
 
     private val clientRegistrationId = "sso"
 
     override fun logout(request: HttpServletRequest, response: HttpServletResponse, authentication: Authentication?) {
-        val authorizedClient: OAuth2AuthorizedClient = oAuth2AuthorizedClientRepository.loadAuthorizedClient(clientRegistrationId, authentication, request)
-                ?: return
-        oauth2Service.logout(authorizedClient)
-        CookieUtils.deleteCookie(request, response, Oauth2Constant.ACCESS_TOKEN)
-        CookieUtils.deleteCookie(request, response, Oauth2Constant.REFRESH_TOKEN)
+        val clientRegistration = clientRegistrationRepository.findByRegistrationId(clientRegistrationId) ?: return
+        val cookies = listOfNotNull(CookieUtils.getCookie(request, Oauth2Constant.ACCESS_TOKEN), CookieUtils.getCookie(request, Oauth2Constant.REFRESH_TOKEN))
+        cookies.forEach {
+            val payload = Oauth2TokenRevokePayload.of(it.value, clientRegistration)
+            oauth2Service.revokeToken(clientRegistration.providerDetails, payload)
+            // 쿠키 제거
+            CookieUtils.deleteCookie(request, response, it.name)
+        }
     }
 }
