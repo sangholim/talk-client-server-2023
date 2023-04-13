ac816032decbf22e118cf9625c4c5be60366e766
hoya <hybriddevslim@gmail.com>
2023-04-13 11:32:51 +0900

    feat: ProfileClient.kt 추가

---
+++ src/main/kotlin/com/talk/client/service/profile/ProfileClient.kt
@@ -0,0 +1,22 @@
+package com.talk.client.service.profile
+
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import reactor.core.publisher.Mono
+
+@Component
+class ProfileClient(
+        private val resourceClient: WebClient
+) {
+    fun get(token: String): ProfileView? = resourceClient.get().uri("/member-service/profiles")
+            .headers {
+                it.setBearerAuth(token)
+            }
+            .retrieve()
+            .bodyToMono(ProfileView::class.java)
+            .onErrorResume(WebClientResponseException::class.java) {
+                if (it.statusCode.value() == HttpStatus.NOT_FOUND.value()) Mono.empty() else Mono.error(it)
+            }.block()
+}
