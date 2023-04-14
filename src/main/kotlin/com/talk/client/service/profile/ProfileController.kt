fefc3919914ce789a84164dd5e2b37f38e431581
hoya <hybriddevslim@gmail.com>
2023-04-14 09:55:29 +0900

    refactor: ProfileController.kt 제거 - 친구 조회 END-POINT 제거

--- src/main/kotlin/com/talk/client/service/profile/ProfileController.kt
+++ src/main/kotlin/com/talk/client/service/profile/ProfileController.kt
@@ -2,8 +2,6 @@ package com.talk.client.service.profile
 
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
@@ -19,20 +17,6 @@ class ProfileController(
     fun create(authentication: JwtAuthenticationToken, payload: ProfileCreationPayload): String {
         val response = profileClient.create(authentication.token.tokenValue, payload)
         if (!response?.statusCode?.is2xxSuccessful!!) throw Exception("계정 생성 실패하였습니다.")
-        return "redirect:/profiles/friends"
-    }
-
-    /**
-     * 프로필이 없는 경우, 생성 화면으로 렌더링
-     */
-    @GetMapping("/profiles/friends")
-    fun friends(authentication: JwtAuthenticationToken, model: Model): String {
-        val profile = profileClient.get(authentication.token.tokenValue)
-        if (profile == null) {
-            model.addAttribute("email", authentication.tokenAttributes["email"])
-            return "profiles/creation"
-        }
-        model.addAttribute("profile", profile)
-        return "profiles/friends"
+        return "redirect:/friends"
     }
 }
