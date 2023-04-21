351254bd1ff5b5f9c4f955ba4b25225a332c065f
hoya <hybriddevslim@gmail.com>
2023-04-21 09:11:33 +0900

    refactor: ProfileView.kt 수정 - 친구 리스트 필드 추가

--- src/main/kotlin/com/talk/client/service/profile/ProfileView.kt
+++ src/main/kotlin/com/talk/client/service/profile/ProfileView.kt
@@ -1,7 +1,10 @@
 package com.talk.client.service.profile
 
+import com.talk.client.service.friend.FriendView
+
 data class ProfileView(
         val id: String,
         val email: String,
-        val name: String
+        val name: String,
+        val friends: List<FriendView>? = emptyList()
 )
