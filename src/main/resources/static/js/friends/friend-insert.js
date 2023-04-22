3cbc1a007be505b3c12af1a27d69300b575e8a49
hoya <hybriddevslim@gmail.com>
2023-04-23 00:12:01 +0900

    refactor: friend-insert.js 수정 - modal data 초기화 로직 리팩토링 - 친구 추가하기 버튼 이벤트 로직 추가

--- src/main/resources/static/js/friends/friend-insert.js
+++ src/main/resources/static/js/friends/friend-insert.js
@@ -1,15 +1,40 @@
 const friend = {
     insert: {
         modalId: 'friend-insert-modal',
-        addEvent : function() {
+        buttonId: 'friend-insert-button',
+        addEvents : function() {
             var modal = document.getElementById(`${this.modalId}`);
             modal.addEventListener('hide.bs.modal', function () {
-                const nodes = document.querySelectorAll(`#${modal.id} .modal-body input`);
-                console.log(this.modalId);
-                nodes.forEach(x=> {
-                    x.value = '';
-                });
-            })
+                friend.insert.modalDataClear(modal);
+            });
+            var insertButton = document.getElementById(`${this.buttonId}`);
+            insertButton.addEventListener('click', function () {
+                friend.insert.api(modal);
+            });
+        },
+        modalDataClear: function(modal) {
+            const nodes = document.querySelectorAll(`#${modal.id} .modal-body input[type='text']`);
+            nodes.forEach(x=> {
+                x.value = '';
+            });
+        },
+        api: function(modal) {
+            const body = {};
+            const nodes = document.querySelectorAll(`#${modal.id} .modal-body input`);
+            nodes.forEach(x=> {
+                body[x.name] = x.value;
+            });
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '/friends');
+            xhr.setRequestHeader('Content-type', 'application/json');
+            xhr.send(JSON.stringify(body));
+            xhr.onload = function (e) {
+              if(xhr.status === 200) {
+                console.log(xhr.responseText);
+              } else {
+                console.log('Error!');
+              }
+            };
         }
     }
 }
