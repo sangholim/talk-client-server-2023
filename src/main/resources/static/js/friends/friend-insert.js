d0146efb9b99f2dc869498468f7d975c1028afb1
sangholim <55565835+sangholim@users.noreply.github.com>
2023-04-24 14:22:12 +0900

    refactor: 친구 추가시 로딩 처리 (#55)
    * feat: /templates/components/spinner.html 추가
    
    * feat: /js/components/spinner.js 추가
    
    * refactor: /js/friends/friend-insert.js 수정
    - 생성 client 요청 로직 수정
    - 요청시 로딩 처리 로직 추가
    
    * refactor: /template/friends/main.html 수정
    - spinner 로직 추가
    
    * refactor: /template/layout/grid-layout.html 수정
    - 불필요한 스크립트 로직 제거

--- src/main/resources/static/js/friends/friend-insert.js
+++ src/main/resources/static/js/friends/friend-insert.js
@@ -1,17 +1,15 @@
 const friend = {
     insert: {
-        modalId: 'friend-insert-modal',
-        buttonId: 'friend-insert-button',
         messageId: 'friend-insert-message',
         defaultMessage: '이메일을 입력하세요',
         addEvents : function() {
-            var modal = document.getElementById(`${this.modalId}`);
+            var modal = document.getElementById('friend-insert-modal');
             modal.addEventListener('hide.bs.modal', function () {
                 friend.insert.modalDataClear(modal);
             });
-            var insertButton = document.getElementById(`${this.buttonId}`);
+            var insertButton = document.getElementById('friend-insert-button');
             insertButton.addEventListener('click', function () {
-                friend.insert.api(modal);
+                friend.insert.request(modal);
             });
         },
         modalDataClear: function(modal) {
@@ -21,28 +19,36 @@ const friend = {
             });
             friend.insert.modifyMessage(friend.insert.defaultMessage);
         },
-        api: function(modal) {
+        request: function(modal) {
             const body = {};
             const nodes = document.querySelectorAll(`#${modal.id} .modal-body input`);
             nodes.forEach(x=> {
                 body[x.name] = x.value;
             });
             var xhr = new XMLHttpRequest();
+            xhr.addEventListener('loadstart', friend.insert.loading);
+            xhr.addEventListener('load', friend.insert.response);
             xhr.open('POST', '/api/friends');
             xhr.setRequestHeader('Content-type', 'application/json');
             xhr.send(JSON.stringify(body));
-            xhr.onload = function (e) {
-              var responseBody = JSON.parse(xhr.responseText)
-              if(xhr.status === 200) {
-                console.log(responseBody);
-              } else {
+
+        },
+        response: function(event) {
+            spinner.hide();
+            var response = event.target;
+            var responseBody = JSON.parse(response.responseText);
+            if(response.status !== 201) {
                 friend.insert.modifyMessage(responseBody.message);
-              }
-            };
+                return
+            }
+            console.log(responseBody);
         },
         modifyMessage: function(text) {
             var message = document.getElementById(`${this.messageId}`);
-            message.innerText = text
+            message.innerText = text;
+        },
+        loading: function() {
+            spinner.show();
         }
     }
 }
