351aad54f33b3d492caab9c25d9db749cee48c6e
hoya <hybriddevslim@gmail.com>
2023-04-24 11:40:38 +0900

    feat: /js/components/spinner.js 추가

---
+++ src/main/resources/static/js/components/spinner.js
@@ -0,0 +1,10 @@
+const spinner = {
+    show: function() {
+        var spinner = document.getElementById('components-spinner');
+            spinner.style.display  = "";
+    },
+    hide: function() {
+        var spinner = document.getElementById('components-spinner');
+            spinner.style.display  = "none";
+    }
+}
