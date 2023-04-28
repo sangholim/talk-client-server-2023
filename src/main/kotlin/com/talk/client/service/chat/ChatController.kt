c2766f4fba7bfe1f75cc34497ea98f03ec4833e5
hoya <hybriddevslim@gmail.com>
2023-04-28 15:25:49 +0900

    refactor: ChatController.kt 수정 - 채팅방 리스트 조회시 api 요청 로직 추가

--- src/main/kotlin/com/talk/client/service/chat/ChatController.kt
+++ src/main/kotlin/com/talk/client/service/chat/ChatController.kt
@@ -1,13 +1,19 @@
 package com.talk.client.service.chat
 
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class ChatController {
+class ChatController(
+        private val chatClient: ChatClient
+) {
 
     @GetMapping(value = ["/chats"])
-    fun chatsView(): String {
+    fun getViews(authentication: JwtAuthenticationToken, model: Model): String {
+        val response = chatClient.getAll(authentication.token.tokenValue)
+        model.addAttribute("chats", response)
         return "/chat/main"
     }
 }
