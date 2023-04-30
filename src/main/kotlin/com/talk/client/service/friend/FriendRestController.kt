66d46075f4b1da9cdf87844380d1f2dc48b9868b
hoya <hybriddevslim@gmail.com>
2023-04-30 10:48:54 +0900

    refactor: FriendRestController.kt 수정 - 친구 리스트 호출 api 추가

--- src/main/kotlin/com/talk/client/service/friend/FriendRestController.kt
+++ src/main/kotlin/com/talk/client/service/friend/FriendRestController.kt
@@ -1,15 +1,14 @@
 package com.talk.client.service.friend
 
+import com.talk.client.service.profile.ProfileClient
 import org.springframework.http.HttpStatus
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class FriendRestController(
-        private val client: FriendClient
+        private val client: FriendClient,
+        private val profileClient: ProfileClient
 ) {
     /**
      * resource 서버로 친구 생성 요청
@@ -18,6 +17,12 @@ class FriendRestController(
     @ResponseStatus(HttpStatus.CREATED)
     fun insert(authentication: JwtAuthenticationToken, @RequestBody payload: FriendCreationPayload): FriendView? {
         return client.create(authentication.token.tokenValue, payload)
+    }
+
+    @GetMapping("/api/friends")
+    fun getProfileFriends(authentication: JwtAuthenticationToken): List<FriendView> {
+        val profile = profileClient.get(authentication.token.tokenValue)
+        return profile?.friends ?: emptyList()
     }
 }
 
