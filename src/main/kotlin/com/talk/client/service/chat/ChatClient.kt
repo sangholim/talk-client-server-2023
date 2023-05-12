337760d85759d6171654aad10e28448d9b2a2750
hoya <hybriddevslim@gmail.com>
2023-05-12 23:24:44 +0900

    refactor: ChatClient.kt 수정 - 프로필 채팅 상세 조회 client 로직 수정

--- src/main/kotlin/com/talk/client/service/chat/ChatClient.kt
+++ src/main/kotlin/com/talk/client/service/chat/ChatClient.kt
@@ -1,5 +1,6 @@
 package com.talk.client.service.chat
 
+import com.talk.client.service.profile.ProfileChatDetailView
 import com.talk.client.service.profile.ProfileChatView
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.stereotype.Component
@@ -23,7 +24,7 @@ class ChatClient(
             .headers {
                 it.setBearerAuth(token)
             }.retrieve()
-            .bodyToMono(ProfileChatView::class.java)
+            .bodyToMono(ProfileChatDetailView::class.java)
             .block()
 
     fun create(token: String, payload: ChatCreationPayload) = resourceClient.post()
