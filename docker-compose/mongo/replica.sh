a950068da0d4eb109efebe14bf4dc5f6b88d7838
sangholim <55565835+sangholim@users.noreply.github.com>
2023-05-12 18:12:45 +0900

    refactor: docker-compose, mongo 개선 (#72)
    - replica.sh 실행시, health-check 을 통행서 실행

--- docker-compose/mongo/replica.sh
+++ docker-compose/mongo/replica.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+RS_STATUS=$(mongosh --quiet localhost:27017 --eval "rs.status().ok")
 
-mongosh <<EOF
-var config = {
-    "_id": "rs0",
-    "version": 1,
-    "members": [
-        {
-            "_id": 1,
-            "host": "mongo:27017",
-            "priority": 1
-        }
-    ]
-};
-rs.initiate(config, { force: true });
-rs.status();
-EOF
+if [[ $RS_STATUS != 1 ]]
+then
+    # replica set configuration
+    cfg="{
+        _id: 'rs0',
+        version: 1,
+        members: [
+            {
+                "_id": 1,
+                "host": 'mongo:27017',
+                "priority": 1
+            }
+        ]
+    }"
+    mongosh localhost:27017 --eval "JSON.stringify(db.adminCommand({'replSetInitiate' : $cfg}))"
+    echo "[INFO] Replication set initiated."
+else
+  echo "[INFO] Replication set already initiated."
+fi
