02f8f4d12ba8fee87323290dbc0457559048add5
hoya <hybriddevslim@gmail.com>
2023-05-14 20:22:53 +0900

    refactor: ProfileChatDetailView.kt 수정

--- src/main/kotlin/com/talk/client/service/profile/ProfileChatDetailView.kt
+++ src/main/kotlin/com/talk/client/service/profile/ProfileChatDetailView.kt
@@ -1,12 +1,31 @@
 package com.talk.client.service.profile
 
+import com.talk.client.service.chatParticipant.ChatParticipantView
 import java.util.*
 
 data class ProfileChatDetailView(
-        val id: UUID,
+        /**
+         * 사용자 채팅 ID
+         */
         val chatId: UUID,
+        /**
+         * 사용자 채팅 참가자 ID
+         */
         val chatParticipantId: UUID,
+        /**
+         * 채팅방 이름
+         */
         val roomName: String,
-        val image: String,
+        /**
+         * 채팅방 이미지
+         */
+        val roomImage: String,
+        /**
+         * 자신을 제외한 참가자들
+         */
+        val participants: List<ChatParticipantView>,
+        /**
+         * 참가자 전체 수
+         */
         val participantCount: Int
 )
