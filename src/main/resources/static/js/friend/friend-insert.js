f3b9b52ecc588b43109962d2015877c6df4b0a79
sangholim <55565835+sangholim@users.noreply.github.com>
2023-05-16 22:56:54 +0900

    refactor: 이름으로 친구 추가 UI 추가 (#79)

--- src/main/resources/static/js/friend/friend-insert.js
+++ src/main/resources/static/js/friend/friend-insert.js
@@ -1,27 +1,142 @@
 const friend = {
+    email: {
+        nav: {
+            get: () => {
+                return document.querySelector('#friend-insert-modal .nav .email')
+            },
+            active: () => {
+                let nav = friend.email.nav.get();
+                nav.classList.add("active");
+                nav.style.color='';
+            },
+            inActive: () => {
+                let nav = friend.email.nav.get();
+                nav.classList.remove("active");
+                nav.style.color='grey';
+            }
+        },
+        form: {
+            get: () => {
+                return document.querySelector('#friend-insert-modal .form .email')
+            },
+            active: () => {
+                let form = friend.email.form.get();
+                form.classList.add("active");
+                form.classList.remove("d-none");
+            },
+            inActive: () => {
+                let form = friend.email.form.get();
+                form.classList.remove("active");
+                form.classList.add("d-none");
+            },
+            fieldClear: () => {
+                const nodes = friend.email.form.get().querySelectorAll(`.modal-body input[type='text']`);
+                nodes.forEach(x=> {
+                    x.value = '';
+                });
+                friend.email.form.message.modify();
+            },
+            message: {
+                _default: '이메일을 입력하세요',
+                get: () => {
+                    return friend.email.form.get().querySelector('.message');
+                },
+                modify: (text) => {
+                    friend.email.form.message.get().innerText = !text? friend.email.form.message._default : text;
+                }
+            }
+        }
+    },
+    name: {
+        nav: {
+            get: () => {
+                return document.querySelector('#friend-insert-modal .nav .name')
+            },
+            active: () => {
+                let nav = friend.name.nav.get();
+                nav.classList.add("active");
+                nav.style.color='';
+            },
+            inActive: () => {
+                let nav = friend.name.nav.get();
+                nav.classList.remove("active");
+                nav.style.color='grey';
+            }
+        },
+        form: {
+            get: () => {
+                return document.querySelector('#friend-insert-modal .form .name')
+            },
+            active: () => {
+                let form = friend.name.form.get();
+                form.classList.add("active");
+                form.classList.remove("d-none");
+            },
+            inActive: () => {
+                let form = friend.name.form.get();
+                form.classList.remove("active");
+                form.classList.add("d-none");
+            },
+            fieldClear: () => {
+                const nodes = friend.name.form.get().querySelectorAll(`.modal-body input[type='text']`);
+                nodes.forEach(x=> {
+                    x.value = '';
+                });
+                friend.name.form.message.modify();
+            },
+            message: {
+                _default: '이름을 입력하세요',
+                get: () => {
+                    return friend.name.form.get().querySelector('.message');
+                },
+                modify: (text) => {
+                    friend.name.form.message.get().innerText = !text? friend.name.form.message._default : text;
+                }
+            }
+        }
+    },
     insert: {
-        messageId: 'friend-insert-message',
-        defaultMessage: '이메일을 입력하세요',
         addEvents : function() {
+            friend.email.nav.get().addEventListener('click', function() {
+                friend.name.form.inActive();
+                friend.name.nav.inActive();
+                friend.name.form.fieldClear();
+
+                friend.email.nav.active();
+                friend.email.form.active();
+                friend.email.form.fieldClear();
+            });
+            friend.name.nav.get().addEventListener('click', function() {
+                friend.email.form.inActive();
+                friend.email.nav.inActive();
+                friend.email.form.fieldClear();
+
+                friend.name.nav.active();
+                friend.name.form.active();
+                friend.name.form.fieldClear();
+            });
             var modal = document.getElementById('friend-insert-modal');
             modal.addEventListener('hide.bs.modal', function () {
-                friend.insert.modalDataClear(modal);
+                friend.name.form.inActive();
+                friend.name.nav.inActive();
+                friend.name.form.fieldClear();
+
+                friend.email.nav.active();
+                friend.email.form.active();
+                friend.email.form.fieldClear();
             });
             var insertButton = document.getElementById('friend-insert-button');
             insertButton.addEventListener('click', function () {
                 friend.insert.request(modal);
             });
         },
-        modalDataClear: function(modal) {
-            const nodes = document.querySelectorAll(`#${modal.id} .modal-body input[type='text']`);
-            nodes.forEach(x=> {
-                x.value = '';
-            });
-            friend.insert.modifyMessage(friend.insert.defaultMessage);
+        activeForm: () => {
+            return document.querySelector(`#friend-insert-modal .modal-body .form .active`);
         },
         request: function(modal) {
             const body = {};
-            const nodes = document.querySelectorAll(`#${modal.id} .modal-body input`);
+            const activeForm = friend.insert.activeForm();
+            const nodes = activeForm.querySelectorAll(`input`);
             nodes.forEach(x=> {
                 body[x.name] = x.value;
             });
@@ -44,7 +159,8 @@ const friend = {
             location.replace(location.href);
         },
         modifyMessage: function(text) {
-            var message = document.getElementById(`${this.messageId}`);
+            const activeForm = friend.insert.activeForm();
+            const message = activeForm.querySelector(`.message`);
             message.innerText = text;
         },
         loading: function() {
